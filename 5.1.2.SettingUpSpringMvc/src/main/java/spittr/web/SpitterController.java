261256746887d6fe5c26b01c516abde0fd849569
spasynkov <s.pasynkov@gmail.com>
2016-12-26 14:59:14 +0200

    Using URL placeholders for escaping characters in username to pass it as model attribute by using redirect.

--- 5.1.2.SettingUpSpringMvc/src/main/java/spittr/web/SpitterController.java
+++ 5.1.2.SettingUpSpringMvc/src/main/java/spittr/web/SpitterController.java
@@ -4,22 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 import spittr.Spitter;
 import spittr.data.SpitterRepository;
 import spittr.exceptions.SpitterNotFoundException;
 
 import javax.servlet.http.Part;
 import javax.validation.Valid;
-
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -65,7 +60,8 @@ public class SpitterController {
         }
 
         spitterRepository.save(spitter);
-        return "redirect:/spitter/" + spitter.getUsername();
+        model.addAttribute("username", spitter.getUsername());
+        return "redirect:/spitter/{username}";
     }
 
     @RequestMapping("/{username}")
